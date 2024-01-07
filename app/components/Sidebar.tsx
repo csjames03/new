47dba132bf9000f957fddbd32ba1d253ee96ebca
csjames03 <csjames0303@gmail.com>
2024-01-07 23:29:40 +0800

    fix: Set SideNav margin

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 const Sidebar = () =>{
     return(
-        <div className=" bg-slate-100 h-full w-56 py-28 absolute top-0">
-            <Link href={'/'} className="flex gap-2 mx-10">
+        <div className=" bg-slate-100 h-full w-56 py-36 absolute top-0">
+            <Link href={'/'} className="flex gap-2 mx-10 mt-20">
                 <Image src={`./dashboard-icon.svg`} width={20} height={20} alt="Dashboard Icon"/>
                 <p className="text-sm font-semibold text-red-500">Dashboard</p>
             </Link>
