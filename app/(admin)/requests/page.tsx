739a182c577feca45b0c31b93aca1dc84f21ec7f
csjames03 <csjames0303@gmail.com>
2024-01-10 01:32:48 +0800

    feat: Implement donations, requests and stocks templates

---
+++ app/(admin)/requests/page.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/app/components/Navbar"
+import Sidebar from "@/app/components/Sidebar"
+
+const Requests = () =>{
+    const user = "admin"
+
+    return(
+        <div >
+            <Navbar/>
+            <Sidebar user={user}/>
+            <div className="absolute right-0 w-full  lg:w-11/12 lg:pl-36 h-full top-0 p-2 pt-24 lg:p-24 bg-transparent">
+                <p className="text-black text-sm font-semibold">Requests</p>  
+            </div>
+        </div>
+    )
+}
+
+
+export default Requests
