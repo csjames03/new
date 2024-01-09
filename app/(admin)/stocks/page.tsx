b64b7f5d2f1c3b666f76da96256e3d9e7bc741a1
csjames03 <csjames0303@gmail.com>
2024-01-10 02:08:04 +0800

    feat: Implement Stocks

--- app/(admin)/stocks/page.tsx
+++ app/(admin)/stocks/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/app/components/Navbar"
 import Sidebar from "@/app/components/Sidebar"
+import StockInfoContainer from "@/app/components/StockInfoContainer"
 
 const Stocks = () =>{
     const user = "admin"
@@ -10,6 +11,16 @@ const Stocks = () =>{
             <Sidebar user={user}/>
             <div className="absolute right-0 w-full  lg:w-11/12 lg:pl-36 h-full top-0 p-2 pt-24 lg:p-24 bg-transparent">
                 <p className="text-black text-sm font-semibold">Stocks</p>  
+                <div className="w-full grid grid-flow-row-dense grid-cols-1 md:grid-cols-3 lg:grid-cols-4 grid-rows-4 gap-2 pl-6 py-2">
+                    <StockInfoContainer />
+                    <StockInfoContainer />
+                    <StockInfoContainer />
+                    <StockInfoContainer />
+                    <StockInfoContainer />
+                    <StockInfoContainer />
+                    <StockInfoContainer />
+                    <StockInfoContainer />
+                </div>
             </div>
         </div>
     )
---
+++ app/components/StockInfoContainer.tsx
@@ -0,0 +1,28 @@
+
+
+const StockInfoContainer = () =>{
+    return(
+        <div className="w-5/6 h-36 bg-red-200 rounded-3xl p-2 mt-4 md:mt-20 flex justify-center pr-3">
+            <div className="w-1/3 h-full relative flex items-center pl-4">
+                <div className="bg-red-400 h-8 w-8 p-2 rounded-md flex justify-center items-center z-10 absolute top-10">
+                    <p className="text-white tracking-tighter">A+</p>
+                </div>
+                <div className="bg-white h-8 w-8 p-2 rounded-md flex justify-center items-start mt-6">
+                    <p>2</p>
+                </div>
+            </div>
+            <div className="w-1/3 h-full flex flex-col justify-evenly">
+                <p className="text-[17px] text-nowrap font-semibold text-red-700 tracking-tight">In Stock</p>
+                <div>
+                    <p className="text-sm font-bold  text-red-700  tracking-tighter">Total</p>
+                    <p className="text-2xl font-semibold text-red-700  tracking-tight">12</p>
+                </div>
+            </div>
+            <div className="w-1/3 h-full flex justify-center items-center ">
+                <p className="text-nowrap text-xs font-semibold tracking-tighter text-red-700">15 March 2022</p>
+            </div>
+        </div>
+    )
+}
+
+export default StockInfoContainer
