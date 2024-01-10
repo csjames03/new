4f33171a8db1f85e8cf769f46536d1e2c37f8fa3
csjames03 <csjames0303@gmail.com>
2024-01-11 00:39:23 +0800

    feat: Implement media query on Recieved Section (Stock Admin)

--- app/(admin)/requests/page.tsx
+++ app/(admin)/requests/page.tsx
@@ -12,24 +12,24 @@ const Requests = () =>{
             <Sidebar user={user}/>
             <div className="absolute right-0 w-full  lg:w-11/12 lg:pl-36 h-full top-0 p-2 pt-24 lg:p-24 bg-transparent">
                 <p className="text-black text-sm font-semibold">Requests</p>
-                <div className="w-full py-2 px-4 flex">
-                    <div className="w-8/12">
+                <div className="w-full py-2 px-4 flex flex-col gap-2 md:flex-row">
+                    <div className="w-full md:w-8/12">
                         <div className="w-full flex h-full bg-blue-200 rounded-xl">
-                            <div className="w-3/12 p-4">
+                            <div className="w-2/12 md:w-3/12 p-4">
                                 <p className="text-blue-600 text-lg font-semibold">Received</p>
-                                <div className=" ml-2 mt-7 flex justify-center items-start flex-col">
-                                    <p className="text-blue-600  tracking-tighter font-semibold text-sm">Total</p>
+                                <div className=" ml-2 mt-7 md:flex md:justify-center items-start flex-col md:mt-[20px]">
+                                    <p className="text-blue-600  tracking-tighter font-semibold text-xs md:text-sm">Total</p>
                                     <p className="text-blue-600 font-semibold tracking-tight text-2xl">12</p>
                                 </div>
                             </div>
-                            <div className="w-9/12 grid grid-flow-row-dense grid-cols-8  gap-2 relative px-10">
+                            <div className="w-10/12 md:w-9/12 grid grid-flow-row-dense grid-cols-8  gap-1 md:gap-2 relative px-2 md:px-10 overflow-y-auto">
                                     {
                                         recieved?.map((data)=>(
                                             <div key={`data.bloodType`+'asdaskdj'} className="h-full w-full flex justify-center items-center flex-col">
-                                               <div className=" bg-blue-400 h-8 w-8 flex justify-center items-center rounded-md z-10">
+                                               <div className=" bg-blue-400  h-6  w-6 md:h-8 md:w-8 flex justify-center items-center rounded-md z-10">
                                                     <p className="text-sm font-semibold text-white tracking-tighter">{data.bloodType}</p>
                                                 </div>
-                                                <div className="bg-white absolute  h-8 w-8 flex justify-center items-center  rounded-md top-20">
+                                                <div className="bg-white absolute h-6  w-6 md:h-8 md:w-8 flex justify-center items-center  rounded-md top-[4.6rem] md:top-20">
                                                     <p className="text-sm font-semibold text-blue-600 tracking-tighter">{data.total}</p>
                                                 </div> 
                                             </div>
@@ -38,7 +38,7 @@ const Requests = () =>{
                             </div>
                         </div>
                     </div>
-                    <div className="w-4/12 flex gap-2 px-5">
+                    <div className="w-full md:w-4/12 flex gap-2 px-5">
                         <DownloadForm title="Printable Form"  description="Downloadable Donor's form" img="download-icon.svg"/>
                         <DownloadForm title="Printable Form"  description="Downloadable Donor's form" img="download-icon.svg"/>
                     </div>
