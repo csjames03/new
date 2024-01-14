dc39dec2a11ff4339392bc75e51a8d68c2fea365
csjames03 <csjames0303@gmail.com>
2024-01-14 23:33:10 +0800

    feat: Implement Update API in blood

--- app/api/blood/route.ts
+++ app/api/blood/route.ts
@@ -27,6 +27,30 @@ export async function POST(request: Request, response: Response){
         return NextResponse.json(blood, {status: 200})
     }catch(error){
         console.log(error);
-        return NextResponse.json(response, {status: 500})
+        return NextResponse.json(error, {status: 500})
+    }
+}
+
+export async function PUT(request: Request, response: Response){
+    try{
+        const bodyText = await request.text()
+        const data = await JSON.parse(bodyText)
+        
+        const blood = await prisma.blood.update({
+            where:{
+                id: data.id
+            },
+            data:{
+                bags: data.bags,
+                location: data.location,
+                rh: data.rh,
+                type: data.type
+            }
+        })
+
+        return NextResponse.json(data, {status: 200})
+    }catch(error){
+        console.log(error);
+        return NextResponse.json(response, {'status': 500})
     }
 }
